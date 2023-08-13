afc1210908ccc5303c632fa14bc518124b1bd773
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-08-14 00:42:16 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (29.04%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    int maxi(vector<int>& candies){
-        int maxim = 0 ; 
-        int n = candies.size() ; 
-        for(int i = 0 ; i< n ; i++){
-            if(candies[i]>=candies[maxim]) maxim = i ; 
+    // int maxi(vector<int>& candies){
+    //     int maxim = 0 ; 
+    //     int n = candies.size() ; 
+    //     for(int i = 0 ; i< n ; i++){
+    //         if(candies[i]>=candies[maxim]) maxim = i ; 
+    //     }
+    //     return maxim ; 
+    // }
+    // vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+    //     int n = candies.size() ;
+    //     vector<bool> result(n) ;  
+    //     for(int i = 0 ; i<n ; i++){
+    //         candies[i]+=extraCandies ; 
+    //         if(i == maxi(candies) || candies[i] == candies[maxi(candies)])result[i] = true ; 
+    //         candies[i]-=extraCandies ;
+    //     }
+    //     return result ; 
+    // }
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies){
+        vector<bool> out ; 
+        int max = *max_element(candies.begin() , candies.end()) ; 
+        for(auto i : candies){
+            if(i+extraCandies >= max) out.push_back(true) ; 
+            else out.push_back(false) ; 
         }
-        return maxim ; 
-    }
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        int n = candies.size() ;
-        vector<bool> result(n) ;  
-        for(int i = 0 ; i<n ; i++){
-            candies[i]+=extraCandies ; 
-            if(i == maxi(candies) || candies[i] == candies[maxi(candies)])result[i] = true ; 
-            candies[i]-=extraCandies ;
-        }
-        return result ; 
+        return out; 
     }
 };
