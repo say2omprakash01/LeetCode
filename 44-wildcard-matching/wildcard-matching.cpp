b9399e3b2d4834e635b70f03734ac0494ce03fca
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-11-04 02:32:29 +0530

    Time: 83 ms (33.10%) | Memory: 7.1 MB (94.49%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
     bool isAllStars(string &S1, int i) {
-     for (int j = 1; j <= i; j++) {
-         if (S1[j - 1] != '*')
+    // S1 is taken in 1-based indexing
+    for (int j = 1; j <= i; j++) {
+        if (S1[j - 1] != '*')
             return false;
-     }
-     return true;
     }
+    return true;
+}
 
-    bool isMatch(string S2, string S1) {
-        int n = S1.size();
-        int m = S2.size();
+// Function to perform wildcard pattern matching between S1 and S2
+bool isMatch(string &S2, string &S1) {
+    int n = S1.size();
+    int m = S2.size();
 
-    // Create a DP table to memoize results
-    vector<vector<bool>> dp(n + 1, vector<bool>(m+1, false));
+    // Create two arrays to store previous and current rows of matching results
+    vector<bool> prev(m + 1, false);
+    vector<bool> cur(m + 1, false);
 
-    // Initialize the first row and column
-    dp[0][0] = true;
-    for (int j = 1; j <= m; j++) {
-        dp[0][j] = false;
-    }
+    prev[0] = true; // Initialize the first element of the previous row to true
+
     for (int i = 1; i <= n; i++) {
-        dp[i][0] = isAllStars(S1, i);
-     }
-
-    // Fill in the DP table
-    for (int i = 1; i <= n; i++) {
+        cur[0] = isAllStars(S1, i); // Initialize the first element of the current row
         for (int j = 1; j <= m; j++) {
-            if (S1[i - 1] == S2[j - 1] || S1[i- 1] == '?') {
-                dp[i][j] = dp[i - 1][j - 1];
+            if (S1[i - 1] == S2[j - 1] || S1[i - 1] == '?') {
+                cur[j] = prev[j - 1]; // Characters match or S1 has '?'
             } else {
                 if (S1[i - 1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    cur[j] = prev[j] || cur[j - 1]; // '*' represents empty or a character
                 } else {
-                    dp[i][j] = false;
+                    cur[j] = false; // Characters don't match and S1[i-1] is not '*'
                 }
             }
         }
+        prev = cur; // Update the previous row with the current row
     }
 
-    // The value at dp[n][m] contains whether S1 matches S2
-       return dp[n][m];
-    }
+    // The value at prev[m] contains whether S1 matches S2
+    return prev[m];
+}
 };
