7d0fc65db9560b91a9d513a19700d1a6b0d06302
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-11-05 16:15:24 +0530

    Time: 13 ms (61.61%) | Memory: 11.3 MB (84.03%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int maxProfit(int k, vector<int>& Arr) {
+        int n = Arr.size() ; 
+        vector<vector<int>> ahead(2, vector<int>(k+1, 0));
+       vector<vector<int>> cur(2, vector<int>(k+1, 0));
+
+     for (int ind = n - 1; ind >= 0; ind--) {
+        for (int buy = 0; buy <= 1; buy++) {
+            for (int cap = 1; cap <= k; cap++) {
+                if (buy == 0) { // We can buy the stock
+                    cur[buy][cap] = max(0 + ahead[0][cap], 
+                                        -Arr[ind] + ahead[1][cap]);
+                }
+
+                if (buy == 1) { // We can sell the stock
+                    cur[buy][cap] = max(0 + ahead[1][cap],
+                                        Arr[ind] + ahead[0][cap - 1]);
+                }
+            }
+        }
+        // Update the ahead state with the current state for the next iteration.
+        ahead = cur;
+     }
+
+     return ahead[0][k];
+    }
+};
