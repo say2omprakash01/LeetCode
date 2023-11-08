af5df0b6aab457316dc448b13519058c49923457
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-11-09 00:38:56 +0530

    Time: 40 ms (24.36%) | Memory: 9.4 MB (28.40%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -3,12 +3,11 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& arr) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
-
         vector<int> dp(n, 1);  
         vector<int> hash(n, 1); 
 
      for (int i = 0; i < n; i++) {
-        hash[i] = i; // Initialize with the current index
+        hash[i] = i;
         for (int prev_index = 0; prev_index < i; prev_index++) {
             if (arr[i] % arr[prev_index] == 0 && 1 + dp[prev_index] > dp[i]) {
                 dp[i] = 1 + dp[prev_index];
