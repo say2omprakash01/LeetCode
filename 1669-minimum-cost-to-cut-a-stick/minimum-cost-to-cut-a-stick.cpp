d5147dde4b987c46d7d7410b7aabacf2ecaae75a
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-11-15 00:14:17 +0530

    Time: 47 ms (68.85%) | Memory: 10.6 MB (71.76%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -3,29 +3,29 @@ public:
     int minCost(int n, vector<int>& cuts) {
         int c = cuts.size() ; 
         cuts.push_back(n);
-    cuts.insert(cuts.begin(), 0);
-    sort(cuts.begin(), cuts.end());
+        cuts.insert(cuts.begin(), 0);
+        sort(cuts.begin(), cuts.end());
 
-    // Create a DP table to store calculated values.
-    vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+        // Create a DP table to store calculated values.
+        vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
 
-    for (int i = c; i >= 1; i--) {
-        for (int j = 1; j <= c; j++) {
-            if (i > j) continue;
+        for (int i = c; i >= 1; i--) {
+            for (int j = 1; j <= c; j++) {
+                if (i > j) continue;
 
-            int mini = INT_MAX;
+                int mini = INT_MAX;
 
-            for (int ind = i; ind <= j; ind++) {
-                // Calculate the cost for making a cut at position 'ind'.
-                int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+                for (int ind = i; ind <= j; ind++) {
+                    // Calculate the cost for making a cut at position 'ind'.
+                    int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
 
-                mini = min(mini, ans);
+                    mini = min(mini, ans);
+                }
+
+                dp[i][j] = mini;
             }
-
-            dp[i][j] = mini;
         }
-    }
 
-    return dp[1][c];
+        return dp[1][c];
     }
 };
