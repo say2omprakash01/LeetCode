b68fb7f008c0cda906543f45c7ddef05a7aa04b1
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-11-19 22:11:12 +0530

    Time: 18 ms (89.38%) | Memory: 16.7 MB (42.33%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -16,7 +16,7 @@ public:
             for(int j = 0; j < V; j++) {
                 if(adj[i][j] == 1 && i != j) {
                     adjLs[i].push_back(j); 
-                    adjLs[j].push_back(i); 
+                    //adjLs[j].push_back(i); 
                 }
             }
         }
