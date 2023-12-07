2e8dd47b1ecca1ea52a285dba76d293ef5513675
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-08 01:57:18 +0530

    Time: 86 ms (85.01%) | Memory: 42.6 MB (64.29%) - LeetSync

--- 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
+++ 1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.cpp
@@ -1,8 +1,7 @@
 class DisjointSet {
 public:
-    vector<int> rank, parent, size;
+    vector<int> parent, size;
     DisjointSet(int n) {
-        rank.resize(n + 1, 0);
         parent.resize(n + 1);
         size.resize(n + 1);
         for (int i = 0; i <= n; i++) {
@@ -15,22 +14,6 @@ public:
         if (node == parent[node])
             return node;
         return parent[node] = findUPar(parent[node]);
-    }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
     }
 
     void unionBySize(int u, int v) {
