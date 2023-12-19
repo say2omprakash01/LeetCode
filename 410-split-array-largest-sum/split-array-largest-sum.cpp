558e90e14e1639dce645b28d6833177ab4004bad
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-19 15:06:11 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (84.27%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -23,31 +23,5 @@ public:
     }
 };
 
-// class Solution {
-// public:
-//     bool f(std::vector<int>& nums, int k, int mid) {
-//         int p = 1, sum = 0;
-//         for (auto e : nums) {
-//             if (sum + e <= mid)
-//                 sum += e;
-//             else {
-//                 p++;
-//                 sum = e;
-//             }
-//         }
-//         return p <= k;
-//     }
+// Book Allocation Problem and Painter's partition are same as this problem
 
-//     int splitArray(std::vector<int>& nums, int k) {
-//         int low = *std::max_element(nums.begin(), nums.end());
-//         int high = std::accumulate(nums.begin(), nums.end(), 0);
-//         while (low <= high) {
-//             int mid = (low + high) / 2;
-//             if (f(nums, k, mid))
-//                 high = mid - 1;
-//             else
-//                 low = mid + 1;
-//         }
-//         return low;
-//     }
-// };
