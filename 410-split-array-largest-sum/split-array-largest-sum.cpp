fd719c35fe1ed0b73613a175aac12d0a886c16fc
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-19 15:04:53 +0530

    Time: 4 ms (47.81%) | Memory: 7.8 MB (35.29%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,53 +1,53 @@
-// class Solution {
-// public:
-//     bool f(vector<int>& nums, int k , int mid){
-//         int p = 1 , sum = 0  ; 
-//         for(auto e : nums){
-//             if(sum+e<=mid) sum+= e ; 
-//             else{
-//                 p++ ; 
-//                 sum = e ; 
-//             }
-//         }
-//         return p<=mid ; 
-//     }
-//     int splitArray(vector<int>& nums, int k) {
-//         int low = *max_element(nums.begin() , nums.end()) ; 
-//         int high = accumulate(nums.begin() , nums.end() , 0) ; 
-//         while(low<=high){
-//             int mid = (low+high)/2 ; 
-//             if(f(nums , k , mid)) high = mid-1 ; 
-//             else low = mid+1 ;  
-//         }
-//         return low ; 
-//     }
-// };
-
 class Solution {
 public:
-    bool f(std::vector<int>& nums, int k, int mid) {
-        int p = 1, sum = 0;
-        for (auto e : nums) {
-            if (sum + e <= mid)
-                sum += e;
-            else {
-                p++;
-                sum = e;
+    bool f(vector<int>& nums, int k , int mid){
+        int p = 1 , sum = 0  ; 
+        for(auto e : nums){
+            if(sum+e<=mid) sum+= e ; 
+            else{
+                p++ ; 
+                sum = e ; 
             }
         }
-        return p <= k;
+        return p<=k ; 
     }
-
-    int splitArray(std::vector<int>& nums, int k) {
-        int low = *std::max_element(nums.begin(), nums.end());
-        int high = std::accumulate(nums.begin(), nums.end(), 0);
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            if (f(nums, k, mid))
-                high = mid - 1;
-            else
-                low = mid + 1;
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin() , nums.end()) ; 
+        int high = accumulate(nums.begin() , nums.end() , 0) ; 
+        while(low<=high){
+            int mid = (low+high)/2 ; 
+            if(f(nums , k , mid)) high = mid-1 ; 
+            else low = mid+1 ;  
         }
-        return low;
+        return low ; 
     }
 };
+
+// class Solution {
+// public:
+//     bool f(std::vector<int>& nums, int k, int mid) {
+//         int p = 1, sum = 0;
+//         for (auto e : nums) {
+//             if (sum + e <= mid)
+//                 sum += e;
+//             else {
+//                 p++;
+//                 sum = e;
+//             }
+//         }
+//         return p <= k;
+//     }
+
+//     int splitArray(std::vector<int>& nums, int k) {
+//         int low = *std::max_element(nums.begin(), nums.end());
+//         int high = std::accumulate(nums.begin(), nums.end(), 0);
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (f(nums, k, mid))
+//                 high = mid - 1;
+//             else
+//                 low = mid + 1;
+//         }
+//         return low;
+//     }
+// };
