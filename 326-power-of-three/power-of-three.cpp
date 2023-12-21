e2962cf17dd6f1ef7ee53392c401f552f201c646
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-21 21:05:51 +0530

    Time: 15 ms (50.30%) | Memory: 6.1 MB (98.58%) - LeetSync

--- 326-power-of-three/power-of-three.cpp
+++ 326-power-of-three/power-of-three.cpp
@@ -3,6 +3,13 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        return fmod(log10(n) / log10(3), 1) == 0;
+       // return fmod(log10(n) / log10(3), 1) == 0;
+       if(n==0) return false;
+        while(n!=1){
+            int r=n%3; // r: remainder
+            n=n/3;
+            if(r!=0) return false;
+        }
+        return true;
     }
 };
