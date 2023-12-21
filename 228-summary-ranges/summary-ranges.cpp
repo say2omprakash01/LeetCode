89a20bffbfab56fcc50a35e2b8565670d87f6639
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-21 20:43:26 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (92.92%) - LeetSync

--- 228-summary-ranges/summary-ranges.cpp
+++ 228-summary-ranges/summary-ranges.cpp
@@ -9,15 +9,15 @@ public:
             ans.push_back(to_string(nums[0])) ;
             return ans ; 
         }
-        for( i = 0 ; i<n-1 ; i++){
-            //if(nums[i+1]== nums[i]+1) i++ ; 
+        for( i = 0 ; i<n-1 ; i++){ 
            if(nums[i+1]!= nums[i]+1){
                 if(i==j)ans.push_back(to_string(nums[i])) ;
                 else if(i>j)ans.push_back(to_string(nums[j])+"->"+to_string(nums[i])) ; 
                 j= i+1 ; 
             }
         }
-        if(nums[n-1]==nums[n-2]+1 and i>j) ans.push_back(to_string(nums[j])+"->"+to_string(nums[n-1])) ; 
+        if(nums[n-1]==nums[n-2]+1 and i>j) 
+           ans.push_back(to_string(nums[j])+"->"+to_string(nums[n-1])) ; 
         else{
             if(i>j)ans.push_back(to_string(nums[j])+"->"+to_string(nums[n-2])) ; 
             ans.push_back(to_string(nums[n-1])) ; 
