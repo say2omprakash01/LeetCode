5e5fdb7d22486e7883b0f0720a18d0c1a6e3b476
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-21 22:53:24 +0530

    Time: 24 ms (93.32%) | Memory: 19.3 MB (95.10%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -5,7 +5,6 @@ public:
         int n = in.size();
         vector<vector<int>> ans;
         int i = 0, j = 0;
-
         while (i < n) {
             // Check if there's an overlap with the next interval
             while (i < n - 1 && in[i][1] >= in[i + 1][0]) {
@@ -13,15 +12,12 @@ public:
                 in[i + 1][1] = max(in[i][1], in[i + 1][1]);
                 i++;
             }
-            
             // Add the merged interval to the result
             ans.push_back({in[j][0], in[i][1]});
-            
             // Move to the next non-overlapping interval
             i++;
             j = i;
         }
-
         return ans;
     }
 };
