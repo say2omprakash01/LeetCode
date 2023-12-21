9469d49708426edc2d072a73d170c9c239851952
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-21 22:01:51 +0530

    Time: 35 ms (33.66%) | Memory: 19.4 MB (56.66%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& in) {
+        sort(in.begin() , in.end()) ; 
+        int n = in.size();
+        vector<vector<int>> ans;
+        int i = 0, j = 0;
+
+        while (i < n) {
+            // Check if there's an overlap with the next interval
+            while (i < n - 1 && in[i][1] >= in[i + 1][0]) {
+                in[i + 1][0] = min(in[i][0], in[i + 1][0]);
+                in[i + 1][1] = max(in[i][1], in[i + 1][1]);
+                i++;
+            }
+            
+            // Add the merged interval to the result
+            ans.push_back({in[j][0], in[i][1]});
+            
+            // Move to the next non-overlapping interval
+            i++;
+            j = i;
+        }
+
+        return ans;
+    }
+};
