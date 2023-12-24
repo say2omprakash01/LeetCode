568a0798c98997a26dad56a71b3473fb32da8ecd
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-24 22:48:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (45.10%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -3,12 +3,12 @@ public:
     int minOperations(string s) {
         int n = s.size(), count = 0 , count1 = 0 ;
         if(n == 1) return 0;
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) { // 0 at even position 
             if(i%2 == 0 and s[i] == '1') count++;
             if(i%2 == 1 and s[i] == '0') count++;
         }
 
-        for(int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) { // 1 at even position
             if(i%2 == 0 and s[i] == '0') count1++;
             if(i%2 == 1 and s[i] == '1') count1++;
         }
