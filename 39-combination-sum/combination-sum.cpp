f7e1e9ac97d4db488992847f06bf5221447e06ef
Om Prakash <112555213+say2omprakash01@users.noreply.github.com>
2023-12-30 20:14:38 +0530

    Time: 8 ms (48.87%) | Memory: 11 MB (98.13%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void help(int i , int t ,vector<int>& c, vector<int>& v, vector<vector<int>> &ans){
+        if(i==c.size()){
+            if(t==0) ans.push_back(v) ; 
+            return ; 
+        }
+
+        if(c[i]<=t){
+            v.push_back(c[i]) ; 
+            help(i , t-c[i] , c , v , ans) ;
+            v.pop_back() ;
+        }
+
+        help(i+1 , t , c , v , ans) ; 
+    }
+    vector<vector<int>> combinationSum(vector<int>& c, int target) {
+        vector<vector<int>> ans ; 
+        vector<int> v ; 
+        help(0 , target , c , v , ans) ; 
+        return ans ; 
+    }
+};
